c0a880840f36c581401a8f276fb71792ecafb9b3
hdcamara <hdcamara@hotmail.com>
2022-12-05 12:27:44 +0000

    05-12

---
+++ index.js
@@ -0,0 +1,30 @@
+const   http = require('http'), //HTTP server
+        path = require('path'),
+        express = require('express'), //Handling HTTP requests & routing
+        fs = require('fs'), //File system functionalities
+        xmlParse = require('xslt-processor').xmlParse, //XML handling
+        xsltProcess = require('xslt-processor').xsltProcess, //XSLT handling
+        router = express(), //Init our router
+        server = http.createServer(router); //Init our server
+        
+        router.use(express.static(path.resolve(__dirname,'views'))); 
+
+router.get('/', function(req, res) {
+
+    res.writeHead(200, {'Content-Type' : 'text/html'});
+
+    let xml = fs.readFileSync('menu.xml', 'utf8'),
+        xsl = fs.readFileSync('menu.xsl', 'utf8');
+
+    xml = xmlParse(xml);
+    xsl = xmlParse(xsl);
+
+    let html = xsltProcess(xml, xsl);
+
+    res.end(html.toString());
+});
+
+server.listen(process.env.PORT || 3000, process.env.IP || "0.0.0.0", function() {
+    const addr = server.address();
+    console.log("Server listening at", addr.address + ":" + addr.port)
+});
